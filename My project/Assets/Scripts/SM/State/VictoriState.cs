3ce54d4c6907944d80dc2a83cf599e4ccb511e58
ChillyWilly59 <120329237+ChillyWilly59@users.noreply.github.com>
2023-05-09 01:43:42 +0400

    1

---
+++ My project/Assets/Scripts/SM/State/VictoriState.cs
@@ -0,0 +1,25 @@
+﻿using SM;
+
+internal class VictoriState : IState {
+    private IState _stateImplementation;
+
+    public VictoriState(StateMachine stateMachine)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Enter()
+    {
+        _stateImplementation.Enter();
+    }
+
+    public void Update()
+    {
+        _stateImplementation.Update();
+    }
+
+    public void Exit()
+    {
+        _stateImplementation.Exit();
+    }
+}
